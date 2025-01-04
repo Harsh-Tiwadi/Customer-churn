9c315fd508d0c8c5962b19b8bbcbfc8ed5fc7415
data-0101 <yo_harsh@outlook.com>
2025-01-04 20:05:55 +0530

    Transform Data

--- transform.py
+++ transform.py
@@ -1,3 +1,20 @@
 from extract import ExtractDataSql
+from dotenv import load_dotenv
+import os
 
-extractor = ExtractDataSql()
+load_dotenv()
+
+host = os.getenv('host')
+user = os.getenv('user')
+password = os.getenv('password')
+database = os.getenv('database')
+
+# Validate configuration
+if not all([host, user, password, database]):
+    raise ValueError("One or more environment variables (host, user, password, database) are not set.")
+else:
+    print("DB config loaded successfully from .env")
+
+extractor = ExtractDataSql(host, user, password, database)
+data = extractor.get_data("select * from marketing")
+print(data.head())
