0058cbd9d23169f16cf5049aa35bea07cc36d103
Harsh-Tiwadi <yo_harsh@outlook.com>
2025-01-30 00:19:28 +0530

    added index.js

---
+++ model/model.py
@@ -0,0 +1,34 @@
+import sys
+import json
+import joblib
+import pandas as pd
+
+var1 = 3
+def main():
+    if len(sys.argv) !=2:
+        raise IndexError('Give proper argument:\n Length not equal to two')
+## load dataframe:
+    try:
+        json_data = sys.argv[1]
+
+        data = json.loads(json_data)
+
+        df = pd.DataFrame(data)
+    except Exception as e:
+        print(e)
+        raise ValueError('Give a proper dataframe')
+
+## if dataframe:
+
+    if isinstance(df, pd.DataFrame):
+        model = joblib.load('saved_svm_model.joblib')
+        result = model.predict(df)
+        return {'data_shape':df.shape, 'result':result}
+    else:
+        raise ValueError('Not a dataframe')
+
+if __name__ == "__main__":
+    res = main()
+    print(json.dumps(res))
+
+sys.stdout.flush()
