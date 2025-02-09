13d7034e71d88e9d009a444480ecd1c4244c2286
Harsh-Tiwadi <yo_harsh@outlook.com>
2025-02-09 22:37:05 +0530

    Update load.py

--- ETL/load.py
+++ ETL/load.py
@@ -6,14 +6,23 @@
 
 import boto3
 
-bucket_name = 'aws-customer-churn-bucket'
-file = 'data/customer churn/Customer_Info.csv'
-s3_path = 'data/customer_info.csv'
+bucket_name = 'aws-s3-data-for-projects'
+
+file1 = 'data/clean_data/clean_data.parquet'
+s3_path1 = 'data/clean_data/clean_data.parquet'
+
+file2 = 'data/model_data/model_data.parquet'
+s3_path2 = 'data/model_data/model_data.parquet'
+
+file3 = 'notebook/note.txt'
+s3_path3 = 'data/note.txt'
 
 s3 = boto3.client('s3')
-s3.upload_file(file, bucket_name, s3_path)
+s3.upload_file(file1, bucket_name, s3_path1)
+s3.upload_file(file2, bucket_name, s3_path2)
+s3.upload_file(file3, bucket_name, s3_path3)
 
-
+print('Complete')
 
 # ## pip install s3fs
 
