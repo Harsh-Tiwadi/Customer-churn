8215ed301146e1f2e3854a6e405f327ae5a5675b
Harsh-Tiwadi <yo_harsh@outlook.com>
2025-02-10 23:42:06 +0530

    predictions

--- test/predict.py
+++ test/predict.py
@@ -2,11 +2,13 @@ import pandas as pd
 import numpy as np
 import json
 import joblib
+import os
 
 from test_data import TestData
 
 # data
 data = pd.read_csv('data/model_data/test_data.csv')
+# print(data.columns.tolist())
 
 def predict_test_data(df:pd.DataFrame):
     obj = TestData(df)
@@ -19,7 +21,9 @@ def predict_test_data(df:pd.DataFrame):
 
 prediction = predict_test_data(data)
 
-if int(input('type 1 for saving prediction')) == 1:
-    prediction.to_csv('data/prediction/prediction.csv')
+if int(input('type 1 for saving prediction: ')) == 1:
+    os.makedirs('data/prediction', exist_ok=True)
+    prediction.to_csv('data/prediction/prediction.csv', index=False)
+    print('+++ saved!')
 else:
-    print('+++ skipped saving predictions')
+    print('--- skipped saving predictions')
--- test/test_data.py
+++ test/test_data.py
@@ -2,17 +2,6 @@ import pandas as pd
 import numpy as np
 import json
 
-data = pd.read_csv('data/model_data/test_data.csv')
-data.drop('Unnamed: 0', axis=1, inplace=True)
-# data.to_csv('data/model_data/test_data.csv')
-# model_data = pd.read_parquet('data/model_data/model_data.parquet')
-# model_data.drop('churn_label', axis=1, inplace=True)
-# # print(data.columns.tolist())
-# # # print(data.head(3))
-# print(model_data.columns.tolist())
-# # print(model_data.head(3))
-
-
 class TestData():
     def __init__(self, df:pd.DataFrame):
         self.df = df
@@ -27,5 +16,8 @@ class TestData():
             print('+++ data validated')
 
 if __name__ == '__main__':
+    data = pd.read_csv('data/model_data/test_data.csv')
+    data.drop('Unnamed: 0', axis=1, inplace=True)
+    # data.to_csv('data/model_data/test_data.csv', index=None)
     obj = TestData(data)
     obj.check()
