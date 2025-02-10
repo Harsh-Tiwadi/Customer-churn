da0dd70356da1d980a01b7d8cbd4424fddb89832
Harsh-Tiwadi <yo_harsh@outlook.com>
2025-02-10 23:24:53 +0530

    update predict.py

--- test/predict.py
+++ test/predict.py
@@ -9,4 +9,17 @@ from test_data import TestData
 data = pd.read_csv('data/model_data/test_data.csv')
 
 def predict_test_data(df:pd.DataFrame):
+    obj = TestData(df)
+    obj.check()
+    model = joblib.load('model/saved_svm_model.joblib')
+    y_pred = model.predict(df)
+    df['pred'] = y_pred
+    print("+++ Added prediction to test_data")
+    return df
 
+prediction = predict_test_data(data)
+
+if int(input('type 1 for saving prediction')) == 1:
+    prediction.to_csv('data/prediction/prediction.csv')
+else:
+    print('+++ skipped saving predictions')
